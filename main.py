5cbf8ecb302a4858b9772d5de53e993d328b9c0f
Russell Tran <tranrl@stanford.edu>
2021-01-11 00:27:32 -0800

    Update submodule, reach point where can call upon submodule

---
+++ main.py
@@ -0,0 +1,39 @@
+from aws_s3_desktop_uploader import desktop_uploader
+print(desktop_uploader.get_file_created("test/iguana.jpg"))
+
+"""
+import json
+import boxsdk as box
+
+
+with open('config.json') as f:
+	config = json.load(f)
+	oauth = box.OAuth2(
+		client_id=config['client_id'],
+		client_secret=config['client_secret'],
+		access_token=config['access_token'] # developer token
+	)
+	client = box.Client(oauth)
+	root_folder = client.folder(folder_id='0')
+	shared_folder = root_folder.create_subfolder('hello_world')
+	uploaded_file = shared_folder.upload('test/iguana.jpg')
+	shared_link = shared_folder.get_shared_link()
+	print(shared_link)
+
+if __name__ == "__main__":
+	print("Hi")
+"""
+
+
+
+# Attempt to get QR code from image
+
+	# If so, query from database the Box folder id for this experiment
+
+# Get file creation date
+
+# Get subfolder id for file creation date, creating it if necessary
+
+# Uplaod to subfolder
+
+# Move to done 
